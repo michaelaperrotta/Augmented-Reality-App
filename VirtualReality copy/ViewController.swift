231289c691c58b63f1e45e1edaae1e365fbeeaa5
michaelaperrotta <perrotta.m@husky.neu.edu>
2018-10-08 17:24:27 -0400

    first commit
    First Git commit, includes ability to add square to virtual environment in View controller.

---
+++ VirtualReality copy/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  VirtualReality
+//
+//  Created by Michaela Perrotta on 9/25/18.
+//  Copyright © 2018 Michaela Perrotta. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.session.run(configuration)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func add(_ sender: Any) {
+        let node = SCNNode()
+        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.magenta
+        node.position = SCNVector3(0,0,0)
+        
+    self.sceneView.scene.rootNode.addChildNode(node)
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+        self.restartSession()
+    }
+    
+    func restartSession() {
+        self.sceneView.session.pause()
+        self.sceneView.scene.rootNode.enumerateChildNodes { (node, _) in node.removeFromParentNode()
+            
+            self.sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+        
+    }
+}
+
+}
+
